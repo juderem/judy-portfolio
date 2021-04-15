68425dd9d52ef8401341a5d7d18d5b8406317d72
Judy Milner <judy.milner@gmail.com>
2021-04-15 14:13:11 +0100

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Home.module.scss';
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <p>Hey, I'm Judy! I'm a web developer based in Stroud, Gloucestershire.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "../../transitions/transitions.scss";
+import Home from '../Home';
+import About from '../About';
+import Projects from '../Projects';
+import styles from './Nav.module.scss';
+
+const AnimatedSwitch = withRouter(({ location }) => (
+  <TransitionGroup>
+    <CSSTransition key={location.key} classNames="slide" timeout={1000}>
+      <Switch location={location}>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+      </Switch>
+    </CSSTransition>
+  </TransitionGroup>
+));
+
+export default function Nav() {
+  return (
+    <div className="functionality">
+      <BrowserRouter>
+        <AnimatedSwitch />
+        <nav className={styles.container}>
+          <p className={styles.name}>JUDY RACHEL MILNER</p>
+          <ul>
+            <li>
+              <Link to="/" style={{textDecoration: 'none'}}>Home</Link>
+            </li>
+            <li>
+              <Link to="/about" style={{textDecoration: 'none'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/projects" style={{textDecoration: 'none'}}>Projects</Link>
+            </li>
+          </ul>
+        </nav>
+      </BrowserRouter>
+    </div>
+  );
+}
