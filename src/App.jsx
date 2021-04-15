aa6f5b7de25144fafebae73221e1b9eb7237c199
Judy Milner <judy.milner@gmail.com>
2021-04-15 14:27:46 +0100

    Routing update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from './components/Nav';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
+      <Home />
     </div>
   );
 }
