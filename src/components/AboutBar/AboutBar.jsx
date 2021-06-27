6e2c8366b06a9ae3413040e7aef9bf090cf18938
juderem <judy.milner@gmail.com>
2021-06-27 13:50:38 +0100

    further nav progree

--- src/components/AboutBar/AboutBar.jsx
+++ src/components/AboutBar/AboutBar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import styles from './AboutBar.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import library from '../../data/fa-library.js';
 import judy from '../../assets/images/JudyMilnerPicture.jpg';
-import Navo from '../Navo';
+import Design from '../../components/Design';
+import Code from '../../components/Code';
+import Exp from '../Exp';
+import Home from '../Home';
+import library from '../../data/fa-library.js';
+import { useState } from 'react';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "../../transitions/transitions.scss";
+import {
+    BrowserRouter,
+    Link,
+    Redirect, 
+    Route,
+    Router,
+    Switch,
+  } from "react-router-dom";
 
 const AboutBar = () => {
     return (
@@ -31,7 +45,33 @@ const AboutBar = () => {
                     </a>
                 </div>
             </div>
-            <Navo />
+            <div className={styles.container}>
+                <div className={styles.navo}>
+                    <BrowserRouter className={styles.router}>
+                        <Link to="/" className={styles.link}>
+                            <h1 className={styles.atitle}>HOME</h1>
+                        </Link>
+                        <Link to="/code" className={styles.link}>
+                            <h1 className={styles.atitle}>CODE</h1>
+                        </Link>
+                        <Link to="/design" className={styles.link}>
+                            <h1 className={styles.atitle}>DESIGN</h1>
+                        </Link>
+                        <Link to="/exp" className={styles.link}>
+                            <h1 className={styles.atitle}>EXP</h1>
+                        </Link>
+                        <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="judy-portfolio">
+                            <Redirect to="/" />
+                        </Route>
+                        <Route exact path="/code" component={Code} />
+                        <Route exact path="/design" component={Design} />
+                        <Route exact path="/exp" component={Exp} />
+                        </Switch>
+                    </BrowserRouter>
+                    </div>
+                </div>
         </div>
     )
 }
